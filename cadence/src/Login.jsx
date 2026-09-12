08d1620d2a77527f4abfeb9587769d22cf0d0f2e
Gikai Andrews <gnaboys@gmail.com>
2026-09-11 22:31:55 -0400

    redid cadence folder with latest vite and installed tailwind

---
+++ cadence/src/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div class="bg-blue-500 text-white p-4">
+      Login
+    </div>
+  )
+}
+
+export default Login
---
+++ cadence/src/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div class="bg-green-500 text-white p-4">
+
+        
+      Register
+    </div>
+  )
+}
+
+export default Register
